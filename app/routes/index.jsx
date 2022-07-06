f3f4ec9fbe7eff78ae8750108e18c387dacca896
imnsbay <imnsbay@gmail.com>
2022-07-06 14:30:27 -0300

    First commit 2

---
+++ app/routes/index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="flex w-screen h-screen">
+      <h1 className="m-auto p-4 bg-green-400 rounded-lg">Welcome to Remix</h1>
+    </div>
+  );
+}
